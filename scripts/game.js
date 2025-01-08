7221f6ccd97d345a43462baab71c46deb15fac15
cajones93 <jhd7ipdm@students.codeinstitute.net>
2025-01-08 23:39:47 +0000

    Added functionality to retrieve data from API

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,11 @@
 let startButton = document.getElementById("startBtn");
 let backButton = document.getElementById("backBtn");
+let quizData;
+let question = document.getElementById("question");
 
-
+// answer buttons
+const answer3Btn = document.getElementById("answer3");
+const answer4Btn = document.getElementById("answer4");
 
 async function getData(API_URL) {
 
@@ -18,25 +22,63 @@ async function getData(API_URL) {
         throw new Error(data.error);
     }
     
+
 }
 
-// startButton.addEventListener("click", (e) => {
+backButton.addEventListener("click", goToHomepage);
 
-//     startGame();
+function goToHomepage(){
+    window.location.href = "index.html"; 
+}
 
-// })
+startButton.addEventListener("click", startGame);
+
+function changeButtons(){
+    startButton.setAttribute("id", "answer1");
+    backButton.setAttribute("id", "answer2");
+    startButton.innerText = "done";
+    answer3Btn.classList.remove("hidden");
+    answer4Btn.classList.remove("hidden");
+    startButton.removeEventListener("click", startGame);
+    backButton.removeEventListener("click", goToHomepage);
+}
+
 
 async function startGame(){
     
     // retrieve quiz data from local storage
     let API_URL = localStorage.getItem("API_URL");
 
+    quizData = await getData(API_URL);
+
+
     console.log(API_URL);
+    console.log(quizData);
 
-    // let quizData = await getData(API_URL);
-    // let question = document.getElementById("quiz-question"); 
-    // let dataQuestions = quizData.questions;
+    const quizQuestions = quizData.results.map(question => ({ 
+        question: question.question, 
+        correctAnswer: question.correct_answer, 
+        incorrectAnswers: question.incorrect_answers 
+      }));
+      
+      console.log("quiz questions: ", quizQuestions); 
 
+      let shuffledQuiz = shuffleQuestions(quizQuestions);
+
+      console.log("shuffled quiz questions: ", shuffledQuiz);
     // question.innerText = dataQuestions[Math.floor(Math.random() * noOfQuestions)];
 
+    changeButtons();
+
 }
+
+
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleQuestions(quizData) {
+    for (let i = quizData.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [quizData[i], quizData[j]] = [quizData[j], quizData[i]]; 
+    }
+
+    return quizData;
+  }
