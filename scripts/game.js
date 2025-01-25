0d6984c03eaa03e51444d30d9e878fb7c8a7cc8f
cajones93 <jhd7ipdm@students.codeinstitute.net>
2025-01-25 13:54:35 +0000

    Adjusted responsive design and cleaned up formatting

--- scripts/game.js
+++ scripts/game.js
@@ -8,7 +8,6 @@ let currentQuestion;
 let quizQuestions;
 
 
-
 // URL to reset session token (add session token after)
 let tokenResetURL = "https://opentdb.com/api_token.php?command=reset&token=";
 
@@ -31,10 +30,14 @@ const currentQuestionContainer = document.getElementById("current-question-conta
 const totalQuestions = document.getElementById("totalQuestions");
 const questionNoText = document.getElementById("questionNo");
 
-// Error Modal
-const errorModal = document.getElementById("errorModalRC4");
-const errorModalClose = document.getElementById("errorModalClose");
+// Error Modal (Session Token)
+const tokenErrorModal = document.getElementById("tokenErrorModal");
 
+// Error Modal (No Parameters)
+const paramsErrorModal = document.getElementById("paramsErrorModal");
+
+
+// Loading Div
 const loadingDiv = document.getElementById("loading-div");
 
 // stop keyboard commands
@@ -43,22 +46,28 @@ window.onload = function setLoading() {
   console.log("interval set");
 };
 
-function loadingFinished(){
+function loadingFinished() {
   loadingDiv.classList.add("hidden");
 }
 
-  function getParams(){
-    // retrieve quiz data from local storage
-    let quizParams = JSON.parse(localStorage.getItem('quizParams'));
+function getParams() {
+  // retrieve quiz data from local storage
 
-    // set the parameters
-    sessionToken = quizParams.token;
-    quizLength = quizParams.quizLength;
-    quizCategory = quizParams.quizCategory;
-    difficulty = quizParams.difficulty;
-    quizType = quizParams.quizType;
+  let quizParams = JSON.parse(localStorage.getItem('quizParams'));
 
-    return quizParams;
+
+  if (localStorage.length < 1) {
+    console.log("Error, no quiz params");
+    paramsErrorModal.style.display = 'block';
+  }
+  // set the parameters
+  sessionToken = quizParams.token;
+  quizLength = quizParams.quizLength;
+  quizCategory = quizParams.quizCategory;
+  difficulty = quizParams.difficulty;
+  quizType = quizParams.quizType;
+
+  return quizParams;
 }
 
 async function getData(API_URL) {
@@ -73,179 +82,169 @@ async function getData(API_URL) {
   const data = await response.json();
 
   console.log("response code: ", data.response_code);
-//   if session token needs to be reset, call the error modal
-  if(data.response_code === 4){
+  //   if session token needs to be reset, call the error modal
+  if (data.response_code === 4) {
     console.log("Response Code 4 Error");
-    errorModal.classList.add("show");
-    errorModal.style.display = "block";
-    errorModalClose.addEventListener("click", goToHomepage);
-  }
-  else {
+    tokenErrorModal.style.display = "block";
+  } else {
     // not response code 4
     return data;
   }
 }
 
 // refresh token called through error modal button
-async function refreshToken(){
-    console.log("Response Code 4 Error called");
-    const resetResponse = await fetch((tokenResetURL + sessionToken), {
-      method: "GET"
-    });
-    console.log(`Token reset successful!"
+async function refreshToken() {
+  console.log("Response Code 4 Error called");
+  const resetResponse = await fetch((tokenResetURL + sessionToken), {
+    method: "GET"
+  });
+  console.log(`Token reset successful!"
       URL: ${tokenResetURL}${sessionToken}`);
-      // reload the page to show loading div and avoid too many api calls
-      location.reload();
-    if (!resetResponse.ok) {
-        throw new Error(`Error resetting session token! status: ${resetResponse.status}`);
-      }
-      return 0;
+  // reload the page to show loading div and avoid too many api calls
+  location.reload();
+  if (!resetResponse.ok) {
+    throw new Error(`Error resetting session token! status: ${resetResponse.status}`);
+  }
+  return 0;
 }
 
 // Initial event listeners
 answer1Btn.addEventListener("click", startGame);
 answer2Btn.addEventListener("click", goToHomepage);
 
-function goToHomepage(){
-    window.location.href = "index.html"; 
-    localStorage.clear();
+function goToHomepage() {
+  window.location.href = "index.html";
+  localStorage.clear();
 }
 
-function changeButtons(quizType){
-    answer1Btn.removeEventListener("click", startGame);
-    answer2Btn.removeEventListener("click", goToHomepage);
+function changeButtons(quizType) {
+  answer1Btn.removeEventListener("click", startGame);
+  answer2Btn.removeEventListener("click", goToHomepage);
 
-    // reveal buttons 3 and 4 for multiple choice quiz
-    if(quizType === "multiple"){
-        answer3Btn.classList.remove("hidden");
-        answer4Btn.classList.remove("hidden");
-    }
+  // reveal buttons 3 and 4 for multiple choice quiz
+  if (quizType === "multiple") {
+    answer3Btn.classList.remove("hidden");
+    answer4Btn.classList.remove("hidden");
+  }
 }
 
 
 
 
-async function startGame(){
-    //get quiz parameters
-    let quizParams = getParams();
-    //get the URL
-    let API_URL = await quizParams.url;
+async function startGame() {
+  //get quiz parameters
+  let quizParams = getParams();
 
-    console.log(API_URL);
-    try{
+  //get the URL
+  let API_URL = await quizParams.url;
+
+  console.log(API_URL);
+  try {
     quizData = await getData(API_URL);
-    } catch (error){
-        console.error("Error:", error);
-    }
-    console.log(quizData);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+  console.log(quizData);
 
-    // map data into quiz questions
-    quizQuestions = quizData.results.map(question => ({
-        question: question.question,
-        correctAnswer: question.correct_answer,
-        incorrectAnswers: question.incorrect_answers,
-      }));
-    
-    console.log("Quiz Questions:", quizQuestions);
-    
-    // remove initial event listeners and reveal buttonsbutton data
-    changeButtons(quizParams.quizType);
-    // add new listeners to check if answer is correct and change question and answers
-    addButtonListeners();
+  // map data into quiz questions
+  quizQuestions = quizData.results.map(question => ({
+    question: question.question,
+    correctAnswer: question.correct_answer,
+    incorrectAnswers: question.incorrect_answers,
+  }));
 
-    console.log("totalQuestions = ", totalQuestions);
-    totalQuestions.innerText = quizLength;
+  console.log("Quiz Questions:", quizQuestions);
+
+  // remove initial event listeners and reveal buttonsbutton data
+  changeButtons(quizParams.quizType);
+  // add new listeners to check if answer is correct and change question and answers
+  addButtonListeners();
+
+  console.log("totalQuestions = ", totalQuestions);
+  totalQuestions.innerText = quizLength;
 
 
-    // Call setQuestionAndAnswers after defining it
-    setQuestionAndAnswers(); 
+  // Call setQuestionAndAnswers after defining it
+  setQuestionAndAnswers();
 }
 
-function addButtonListeners(){
-    let answerButtons = document.getElementsByClassName("answer-button");
-    for(const button of answerButtons){
-        button.addEventListener("click", (e) => {
-            if (button.innerText === currentQuestion.correctAnswer){
-                score ++;
-                setQuestionAndAnswers();
-            }
-            else {
-                setQuestionAndAnswers();
-            }
-        });
-    }
+function addButtonListeners() {
+  let answerButtons = document.getElementsByClassName("answer-button");
+  for (const button of answerButtons) {
+    button.addEventListener("click", (e) => {
+      if (button.innerText === currentQuestion.correctAnswer) {
+        score++;
+        setQuestionAndAnswers();
+      } else {
+        setQuestionAndAnswers();
+      }
+    });
+  }
 }
 
 async function setQuestionAndAnswers() {
-    // After final question
-    if (questionNo >= quizLength){
+  // After final question
+  if (questionNo >= quizLength) {
 
-        showScore();
-      return 0;
+    showScore();
+    return 0;
+  }
+
+  currentQuestionContainer.style.display = "flex";
+  questionNoText.innerText = questionNo + 1;
+
+  currentQuestion = quizQuestions[questionNo];
+
+  console.log("current question: ", currentQuestion);
+  // set question text
+
+  question.innerHTML = currentQuestion.question;
+
+  let allAnswers = currentQuestion.incorrectAnswers.concat([currentQuestion.correctAnswer]); // Combine incorrect and correct answers
+
+
+
+  // Shuffle the answers array if multiple choice
+  if (quizType === "multiple") {
+    for (let i = allAnswers.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [allAnswers[i], allAnswers[j]] = [allAnswers[j], allAnswers[i]];
     }
-    
-    currentQuestionContainer.style.display = "flex";
-    questionNoText.innerText = questionNo + 1;
-
-    currentQuestion = quizQuestions[questionNo];
-
-    console.log("current question: ", currentQuestion);
-    // set question text
-
-    question.innerHTML = currentQuestion.question;
-
-    let allAnswers = currentQuestion.incorrectAnswers.concat([currentQuestion.correctAnswer]); // Combine incorrect and correct answers
-    
-
-
-    // Shuffle the answers array if multiple choice
-    if (quizType === "multiple"){
-      for (let i = allAnswers.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [allAnswers[i], allAnswers[j]] = [allAnswers[j], allAnswers[i]];
-      }
     // Assign answers to buttons
-      answer1Btn.innerHTML = allAnswers[0];
-      answer2Btn.innerHTML = allAnswers[1];
-      answer3Btn.innerHTML = allAnswers[2];
-      answer4Btn.innerHTML = allAnswers[3];
-    }
-    else{
-      // If true / false, keep in same positions
-      answer1Btn.innerHTML = "True";
-      answer2Btn.innerHTML = "False";
-    }
-  
-
-  
-    questionNo++;
+    answer1Btn.innerHTML = allAnswers[0];
+    answer2Btn.innerHTML = allAnswers[1];
+    answer3Btn.innerHTML = allAnswers[2];
+    answer4Btn.innerHTML = allAnswers[3];
+  } else {
+    // If true / false, keep in same positions
+    answer1Btn.innerHTML = "True";
+    answer2Btn.innerHTML = "False";
   }
 
-  function calculateScorePercent(score){
-    if(score === 0){
-      return "0.00";
-    }
-    let percentage = parseFloat((score / quizLength) * 100).toFixed(2);
 
-    return percentage;
+
+  questionNo++;
+}
+
+function calculateScorePercent(score) {
+  if (score === 0) {
+    return "0.00";
   }
+  let percentage = parseFloat((score / quizLength) * 100).toFixed(2);
 
-  function showScore(){
+  return percentage;
+}
 
-    let scorePercent = calculateScorePercent(score);
-    scoreModalBody.innerHTML = `Score: <strong>${score}</strong>
+function showScore() {
+
+  let scorePercent = calculateScorePercent(score);
+  scoreModalBody.innerHTML = `Score: <strong>${score}</strong>
     <br>
     Percentage: <strong>${scorePercent}%</strong>
     <br>
     <br>
     <span class="small-modal-text">Click the button below to return to the menu</span>`;
 
-    console.log(scorePercent);
-    scoreModal.classList.add("show");
-    scoreModal.style.display = "block";
-
-    scoreModalClose.addEventListener("click", goToHomepage);
-  }
-
-
- 
+  console.log(scorePercent);
+  scoreModal.style.display = "block";
+}
--- scripts/index.js
+++ scripts/index.js
@@ -5,92 +5,89 @@ const selectElements = document.querySelectorAll('select');
 
 // check parameters are selected each time user changes an option, enable submit button when all parameters are selected
 selectElements.forEach(select => {
-    select.addEventListener("change", () => {
-      if (allParamsSelected()) {
-          submitButton.classList.remove("disabled"); 
-      } else {
-        submitButton.classList.add("disabled"); 
-      }
-    });
+  select.addEventListener("change", () => {
+    if (allParamsSelected()) {
+      submitButton.classList.remove("disabled");
+    } else {
+      submitButton.classList.add("disabled");
+    }
   });
+});
 
 // check that user has selected a parameter
 function allParamsSelected() {
-    return Array.from(selectElements).every(select => select.value !== select.options[0].value);
-  }
+  return Array.from(selectElements).every(select => select.value !== select.options[0].value);
+}
 
 submitButton.addEventListener("click", async (e) => {
-    e.preventDefault(); // Prevent default form submission behavior
-    try {
-      await getAPIURL();
-      window.location.href = "game.html"; // Redirect to game page after successful submission
-    } catch (error) {
-      console.error("Error:", error);
-      // Handle errors (e.g., display an error message to the user)
-    }
+  e.preventDefault(); // Prevent default form submission behavior
+  try {
+    await getAPIURL();
+    window.location.href = "game.html"; // Redirect to game page after successful submission
+  } catch (error) {
+    console.error("Error:", error);
+    // Handle errors (e.g., display an error message to the user)
+  }
 });
 
 // generate a session token
-async function getSessionToken(){
-    const response = await fetch("https://opentdb.com/api_token.php?command=request");
+async function getSessionToken() {
+  const response = await fetch("https://opentdb.com/api_token.php?command=request");
 
-    const sessionToken = await response.json();
+  const sessionToken = await response.json();
 
-    const sessionTokenValue = sessionToken.token;
+  const sessionTokenValue = sessionToken.token;
 
-    if (response.ok) {
-        console.log(sessionTokenValue);
-        return sessionTokenValue;
-    } else {
-        throw new Error(sessionToken.error);
-    }
+  if (response.ok) {
+    console.log(sessionTokenValue);
+    return sessionTokenValue;
+  } else {
+    throw new Error(sessionToken.error);
+  }
 }
 
 // build the api url and pass it into local storage for the game page to retrieve
-async function getAPIURL(){
+async function getAPIURL() {
 
 
-    let noOfQuestions = document.getElementById("noOfQuestions");
-    let categoryChoice = document.getElementById("categoryChoice");
-    let difficultyChoice = document.getElementById("difficultyChoice");
-    let quizTypeChoice = document.getElementById("quizTypeChoice");
+  let noOfQuestions = document.getElementById("noOfQuestions");
+  let categoryChoice = document.getElementById("categoryChoice");
+  let difficultyChoice = document.getElementById("difficultyChoice");
+  let quizTypeChoice = document.getElementById("quizTypeChoice");
 
-    let categoryID = setCategoryID(categoryChoice.value);
+  let categoryID = setCategoryID(categoryChoice.value);
 
-    // Wait for session token
-    const sessionToken = await getSessionToken(); // Await the promise
+  // Wait for session token
+  const sessionToken = await getSessionToken(); // Await the promise
 
-    // build the api url using the session token
-    let API_URL = `https://opentdb.com/api.php?amount=${noOfQuestions.value}&category=${categoryID}&difficulty=${difficultyChoice.value}&type=${quizTypeChoice.value}&token=${sessionToken}`;
+  // build the api url using the session token
+  let API_URL = `https://opentdb.com/api.php?amount=${noOfQuestions.value}&category=${categoryID}&difficulty=${difficultyChoice.value}&type=${quizTypeChoice.value}&token=${sessionToken}`;
 
-    
-    const quizParams = {
-        url: API_URL,
-        token: sessionToken,
-        quizLength: noOfQuestions.value,
-        quizCategory: categoryChoice.value,
-        difficulty: difficultyChoice.value,
-        quizType: quizTypeChoice.value 
-      };
 
-    //set data into session storage
-    localStorage.setItem("quizParams", JSON.stringify(quizParams));
+  const quizParams = {
+    url: API_URL,
+    token: sessionToken,
+    quizLength: noOfQuestions.value,
+    quizCategory: categoryChoice.value,
+    difficulty: difficultyChoice.value,
+    quizType: quizTypeChoice.value
+  };
 
-      return 0;
+  //set data into session storage
+  localStorage.setItem("quizParams", JSON.stringify(quizParams));
+
+  return 0;
 }
 
 // assign the correct category ID based on category choice
-function setCategoryID (categoryChoice){
-    if(categoryChoice === "generalKnowledge"){
-        return 9;
-    }
-    else if(categoryChoice === "videoGames"){
-        return 15;
-    }
-    else if(categoryChoice === "music"){
-        return 12;
-    }
-    else if(categoryChoice === "scienceAndNature"){
-        return 17;
-    }
+function setCategoryID(categoryChoice) {
+  if (categoryChoice === "generalKnowledge") {
+    return 9;
+  } else if (categoryChoice === "videoGames") {
+    return 15;
+  } else if (categoryChoice === "music") {
+    return 12;
+  } else if (categoryChoice === "scienceAndNature") {
+    return 17;
+  }
 }
